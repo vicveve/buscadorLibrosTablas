1b64dca1e57f276b1fb113d472366cf285e92cdd
vicveve <victor@macbook-pro-de-victor.local>
2017-04-24 15:00:55 -0500

    Initial Commit

---
+++ buscadorLibrosTablas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buscadorLibrosTablas
+//
+//  Created by Victor Ernesto Velasco Esquivel on 24/04/17.
+//  Copyright © 2017 Victor Ernesto Velasco Esquivel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
